aa527368ea6d8a179e06ea3708174846897e245c
Camila Zagal <camilazags@gmail.com>
2022-10-28 13:04:19 -0500

    added basic files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./pages/Navbar";
+import LandingPage from "./pages/LandingPage";
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+
+}
+
+export default App;
