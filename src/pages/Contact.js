15e3914ad3a8892e901c9ab8de6368503e7d4519
Camila Zagal <camilazags@gmail.com>
2022-11-01 16:10:40 -0600

    added placeholder txt

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,3 +1,9 @@
 export default function Contact() {
-    return <h1>Contact</h1>
+    return (
+        <>
+            <h1 className="contact-title">Contact</h1>
+            <p className="contact-p">Eu pariatur ea voluptate dolore eu nostrud eiusmod sint eiusmod enim aute do. Labore voluptate non dolor consectetur. Proident magna nostrud do sit non duis duis labore incididunt proident labore. Pariatur tempor fugiat officia ea ex incididunt duis sint eiusmod officia sint. Veniam sint esse et esse deserunt occaecat magna veniam proident quis sit laboris culpa id. Non elit aliqua sit ad officia. Mollit sunt occaecat voluptate elit nisi.</p>
+            <p className="contact-p">Id incididunt labore ex sit ex est occaecat. Aliquip eiusmod mollit ad proident aute exercitation consequat laboris. Aute est eu dolor cillum sint ipsum irure enim magna eiusmod est voluptate. Veniam velit elit veniam duis sit exercitation sunt quis pariatur ex duis est enim.</p>
+        </>
+    )
 }
