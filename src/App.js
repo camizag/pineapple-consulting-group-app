d14c7d94f76865682f2e8cd4cd24022315d2e69e
Camila Zagal <camilazags@gmail.com>
2022-11-01 14:06:19 -0600

    added routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import Navbar from "./pages/Navbar";
+import Navbar from './Navbar';
+import Services from "./pages/Services";
 import LandingPage from "./pages/LandingPage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </div>
     </>
-  )
-
+    
+  );
 }
 
 export default App;
