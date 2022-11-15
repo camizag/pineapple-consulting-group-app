94b77b96e4c89ea68018fd518c5e3d20844313ad
Camila Zagal <camilazags@gmail.com>
2022-11-15 15:32:19 -0600

    added about text

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,27 @@
 export default function About() {
     return (
         <>
-            <h1 className="about-title">About</h1>
-            <p className="about-p">Eu pariatur ea voluptate dolore eu nostrud eiusmod sint eiusmod enim aute do. Labore voluptate non dolor consectetur. Proident magna nostrud do sit non duis duis labore incididunt proident labore. Pariatur tempor fugiat officia ea ex incididunt duis sint eiusmod officia sint. Veniam sint esse et esse deserunt occaecat magna veniam proident quis sit laboris culpa id. Non elit aliqua sit ad officia. Mollit sunt occaecat voluptate elit nisi.</p>
-            <p className="about-p">Id incididunt labore ex sit ex est occaecat. Aliquip eiusmod mollit ad proident aute exercitation consequat laboris. Aute est eu dolor cillum sint ipsum irure enim magna eiusmod est voluptate. Veniam velit elit veniam duis sit exercitation sunt quis pariatur ex duis est enim.</p>
+            <div className="about-container">
+                <h1 className="about-title">ABOUT</h1>
+                <h2 className="about-subtitle">WHO WE ARE?</h2>
+                <p className="about-p">Somos un grupo interdiciplinado de profesionales de distintas industrias que apoyamos a personas y empresas a desarrollarse a su máximo potencial a través del logro de sus objetivos.</p>
+
+                <h2 className="about-subtitle">BHAG (Big Hairy Audacious Goals)</h2>
+                <p className="about-p">Ser una de las 5 principales consultoras a nivel mundial apoyando a pequeños y medianos negocios a ser exitosos.</p>
+
+                <h2 className="about-subtitle">VALUES</h2>
+                <p className="about-p">Responsabilidad</p>
+                <p className="about-p">Empatía</p>
+                <p className="about-p">Respeto</p>
+                <p className="about-p">Honestidad</p>
+                <p className="about-p">Igualdad</p>
+
+                <h2 className="about-subtitle">HISTORY</h2>
+                <p className="about-p">Después de varios años colaborando en las áreas de Marketing y RR.HH con medianas y grandes empresas tanto en USA como México, se junta un grupo de profesionales buscando ayudar a las personas a progresar y a vivir una vida más feliz a través del logro de sus proyectos y de sus objetivos.</p>
+
+                <h2 className="about-subtitle">WHY PINEAPPLE?</h2>
+                <p className="about-p">Escogimos la piña como símbolo de nuestra empresa porque, además de tener corona y varios otros significados que tienen las piñas (hay más en la página) representa estatus, abundancia y hospitalidad, valores que siempre promovemos al involucrarnos en cualquier proyecto.</p>
+            </div>
         </>
     
     )
