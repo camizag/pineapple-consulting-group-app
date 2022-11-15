da27aa5820f7c28eed92f86bb25dcf0bc9001eb6
Camila Zagal <camilazags@gmail.com>
2022-11-15 15:43:30 -0600

    added routes for cards

--- src/components/LandingCards.js
+++ src/components/LandingCards.js
@@ -5,6 +5,8 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 
+import { Link } from "react-router-dom";
+
 
 export default function LandingCards() {
     return (
@@ -13,6 +15,7 @@ export default function LandingCards() {
             
             <div className='landing-cards-container'>
                 <Card  className="landing-cards">
+                <Link className='navbar-link' to="/real-state">
                         <CardMedia
                         component="img"
                         height="230"
@@ -24,12 +27,12 @@ export default function LandingCards() {
                         <Typography className='card-txt' gutterBottom variant="h5">
                             Real State
                         </Typography>
-
-
                         </CardContent>
+                </Link>
                 </Card>
 
                 <Card  className="landing-cards">
+                <Link className='navbar-link' to="/development">
                         <CardMedia
                         component="img"
                         height="230"
@@ -41,12 +44,12 @@ export default function LandingCards() {
                         <Typography className='card-txt' gutterBottom variant="h5">
                             Development
                         </Typography>
-
-
                         </CardContent>
+                </Link>
                 </Card>
 
                 <Card className="landing-cards">
+                <Link className='navbar-link' to="/business-planning">
                         <CardMedia
                         component="img"
                         height="230"
@@ -54,17 +57,16 @@ export default function LandingCards() {
                         image="https://images.unsplash.com/photo-1460317442991-0ec209397118?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
                         alt="green iguana"
                         />
-                        
                         <CardContent className='card-txt-container'>
                         <Typography className='card-txt' gutterBottom variant="h5">
                             Consulting for Strategic Business Planning
                         </Typography>
-
-
                         </CardContent>
+                </Link>
                 </Card>
 
                 <Card  className="landing-cards">
+                <Link className='navbar-link' to="/professional-development">
                         <CardMedia
                         component="img"
                         height="230"
@@ -76,12 +78,12 @@ export default function LandingCards() {
                         <Typography className='card-txt' gutterBottom variant="h5">
                             Career / Professional Development 
                         </Typography>
-
-
                         </CardContent>
+                </Link>
                 </Card>
 
                 <Card  className="landing-cards">
+                <Link className='navbar-link' to="/non-profit">
                         <CardMedia
                         component="img"
                         height="230"
@@ -93,9 +95,8 @@ export default function LandingCards() {
                         <Typography className='card-txt' gutterBottom variant="h5">
                             Non-profit Work
                         </Typography>
-
-
                         </CardContent>
+                </Link>
                 </Card>
                 
                 
