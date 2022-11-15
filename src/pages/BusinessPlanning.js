93a975e0b9ce279585b2ee69758e4e4edf277360
Camila Zagal <camilazags@gmail.com>
2022-11-14 20:16:40 -0600

    added services' pages

---
+++ src/pages/BusinessPlanning.js
@@ -0,0 +1,10 @@
+export default function BusinessPlanning() {
+    return (
+        <>
+            <h1 className="business-planning-title">Consulting for Stratetic Business Planning</h1>
+            <p className="business-planning-p">Eu pariatur ea voluptate dolore eu nostrud eiusmod sint eiusmod enim aute do. Labore voluptate non dolor consectetur. Proident magna nostrud do sit non duis duis labore incididunt proident labore. Pariatur tempor fugiat officia ea ex incididunt duis sint eiusmod officia sint. Veniam sint esse et esse deserunt occaecat magna veniam proident quis sit laboris culpa id. Non elit aliqua sit ad officia. Mollit sunt occaecat voluptate elit nisi.</p>
+            <p className="business-planning-p">Id incididunt labore ex sit ex est occaecat. Aliquip eiusmod mollit ad proident aute exercitation consequat laboris. Aute est eu dolor cillum sint ipsum irure enim magna eiusmod est voluptate. Veniam velit elit veniam duis sit exercitation sunt quis pariatur ex duis est enim.</p>
+        </>
+    
+    )
+}
---
+++ src/pages/Development.js
@@ -0,0 +1,10 @@
+export default function Development() {
+    return (
+        <>
+            <h1 className="development-title">Development</h1>
+            <p className="development-p">Eu pariatur ea voluptate dolore eu nostrud eiusmod sint eiusmod enim aute do. Labore voluptate non dolor consectetur. Proident magna nostrud do sit non duis duis labore incididunt proident labore. Pariatur tempor fugiat officia ea ex incididunt duis sint eiusmod officia sint. Veniam sint esse et esse deserunt occaecat magna veniam proident quis sit laboris culpa id. Non elit aliqua sit ad officia. Mollit sunt occaecat voluptate elit nisi.</p>
+            <p className="development-p">Id incididunt labore ex sit ex est occaecat. Aliquip eiusmod mollit ad proident aute exercitation consequat laboris. Aute est eu dolor cillum sint ipsum irure enim magna eiusmod est voluptate. Veniam velit elit veniam duis sit exercitation sunt quis pariatur ex duis est enim.</p>
+        </>
+    
+    )
+}
