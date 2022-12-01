c9298e47cafd58a5631707e2e638c27a94c28111
Camila Zagal <camilazags@gmail.com>
2022-11-30 19:19:42 -0600

    added contact section

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,7 +1,29 @@
+import { Button, Box } from '@mui/material';
+
+
 export default function ContactSection() {
     return (
         <>
-            
+            <div className='contact-section-container'>
+                <div className='contact-section-text'>
+                    <h1>Minim ipsum pariatur culpa mollit anim minim veniam velit.</h1>
+                </div>
+
+                <div className='contact-section-btn' >
+                <Button  className='contact-btn' display="flex" variant='contained'
+                    sx={{
+                        color: "#ffffff",
+                        backgroundColor: "#FAC136",
+                        ':hover': {
+                        bgcolor: '#FAC136', 
+                        color: 'white',
+                        }
+                    }}
+                    >
+                    MAS INFORMACIÓN 
+                </Button>
+                </div>
+            </div>
         </>
     )
 }
